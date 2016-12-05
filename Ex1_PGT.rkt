7382074f9e3b5b360e9ab2b16e510eacb7c40dab
Angad Gill <angadpalsingh92@gmail.com>
2016-12-05 11:24:01 +0530

    Add Expression

---
+++ Ex1_PGT.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex1_PGT) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+;This expression gives the length of the
+;hypotenuse of a right triangle with sides 3 and 4
+
+(sqrt (+ (sqr 3) (sqr 4)))
