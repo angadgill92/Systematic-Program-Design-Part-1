2a1eb4e8b592ee297c8c3b8d6cf3e3e618a33d8c
Angad Gill <angadpalsingh92@gmail.com>
2016-12-06 15:17:30 +0530

    Add Problem 12

---
+++ BSL_P12.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P12) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+; fixed, add require expression
+
+(rectangle 10 10 "solid" "red")
+; fixed, supplied height argument
+; fixed, changed the style and color args to strings
