e7836bcc7ce7e408795ad87bc14534856ad7bfab
Angad Gill <angadpalsingh92@gmail.com>
2016-12-06 10:48:07 +0530

    Add Problem 3

--- BSL_P3.rkt
+++ BSL_P3.rkt
@@ -1,11 +1,4 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
-(require 2htdp/image)
-
-(define (S color)
-  (square 20 "solid" color))
-
-(above (beside (S "blue") (S "yellow"))
-       (beside (S "yellow") (S "blue")))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSD_P3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(string-append "Super" " " "Glue")
