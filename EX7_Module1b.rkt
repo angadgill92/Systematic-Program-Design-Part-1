489a121321d4ab0879596487f1ee783e6de75c65
Angad Gill <angadpalsingh92@gmail.com>
2016-12-07 17:18:41 +0530

    Add HtDF Exercise 7

---
+++ EX7_Module1b.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname EX7_Module1b) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; Image -> Boolean
+;; produce true if the image is tall
+
+(check-expect (tall? (rectangle 20 30 "solid" "red")) true)
+(check-expect (tall? (rectangle 20 20 "solid" "red")) false)
+(check-expect (tall? (rectangle 30 20 "solid" "red")) false)
+
+;; (define (tall? img) false) ;[Stub]
+;; (define (tall? img) (... img)) ;[Template]
+
+(define (tall? img)
+  (if (> (image-height img) (image-width img))
+      true
+      false))
